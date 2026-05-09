92606a6d74a6a8bb6836f87378a06a1a1093e2ca
amanrajput99 <amanbxr009@gmail.com>
2026-05-09 20:00:59 +0530

    solve some issue

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -64,7 +64,7 @@ const Footer = () => {
                                         <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" /><line x1="17.5" x2="17.51" y1="6.5" y2="6.5" /></svg>
                                 </a>
                                 {/* Youtube */}
-                                <a href="https://www.youtube.com/@EduCrush" className="text-white hover:text-gray-300">
+                                <a href="https://www.youtube.com/@educrush_99" className="text-white hover:text-gray-300">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                         <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17" /><path d="m10 15 5-3-5-3z" />
                                     </svg>
